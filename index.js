d2326e295e2b8c053b69e8adc7cbb15adae66ce0
shahrukmaharuj <shahrukmaharuj10@gmail.com>
2023-09-23 16:26:07 +0600

    add .env

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello Express!');
+require('dotenv').config();
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 5500;
+
+app.get('/', (req, res) => {
+    res.send('Hello Express!')
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+});
